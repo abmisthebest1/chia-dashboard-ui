95f1f66db195b0ffccf8e4f85abebb7652b101d6
abmisthebest1 <65694595+abmisthebest1@users.noreply.github.com>
2021-07-01 16:05:08 -0400

    Update config.ts

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,9 +1,9 @@
-export const discordClientId = '834108043687231570';
+export const discordClientId = '860196793471598602';
 // export const githubClientId = '16645067d609075c61e4';
 export const githubClientId = null;
 // export const googleClientId = '444774972531-atkv3k11o8satjic5m88ldv16beri3rf.apps.googleusercontent.com';
-export const googleClientId = null;
-export const apiBaseUrl = 'https://chia-dashboard-api.foxypool.io/api';
+export const googleClientId = '444774972531-atkv3k11o8satjic5m88ldv16beri3rf.apps.googleusercontent.com';
+export const apiBaseUrl = 'https://vinlexpool.pages.dev/api';
 export const enablePeriodicUpdates = true;
 export const requestDiscordGuildPermission = true;
 
